5eeb6cc287fbd3770dd41219bb70c83a7e821f36
Sagi Turgeman <rrtu44@gmail.com>
2024-02-20 22:03:44 +0200

    modified backend to s3 bucket

--- provider.tf
+++ provider.tf
@@ -1,4 +1,14 @@
 provider "aws" {
   region     = var.region
   allowed_account_ids = ["975050315907"]
+  
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state1234"
+    key            = "terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-locking-table"
+  }
 }
